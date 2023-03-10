0dd23f242b0b43f66310cddb2f141589ccbf1462
chourouk01 <127491299+chourouk01@users.noreply.github.com>
2023-03-10 13:41:06 +0100

    updates front

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,9 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RegisterComponent } from './Authentification/register/register.component';
+import { LoginComponent } from './Authentification/login/login.component';
+import { HeaderComponent } from './Layout/header/header.component';
+import { FooterComponent } from './Layout/footer/footer.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    
+   
   ],
   imports: [
     BrowserModule,
