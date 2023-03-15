54704683ada1c6f2c9bdd5aaf113a355c6069706
chourouk01 <127491299+chourouk01@users.noreply.github.com>
2023-03-15 23:29:48 +0100

    GetById et edit et getAll pour développeur confirm

--- front/src/app/components/ProfilPoste/ProfilDev/DevConfirme/listingdevc/listingdevc.component.ts
+++ front/src/app/components/ProfilPoste/ProfilDev/DevConfirme/listingdevc/listingdevc.component.ts
@@ -10,56 +10,57 @@ import { DataService } from 'src/app/shared/service/data.service';
 export class ListingdevcComponent implements OnInit {
 
 
-  public Idproposte
-  public Profilposte
+  public Idpropostedc
+  public Profilpostedc
 
 
-  public DevJForm = new FormGroup({
-    majn: new FormControl('', [Validators.required]),
-    du: new FormControl('', [Validators.required]),
-    Fonction: new FormControl('', [Validators.required]),
-    Categorie: new FormControl('', [Validators.required]),
-    Suphier: new FormControl('', [Validators.required]),
-    Super: new FormControl('', [Validators.required]),
-    Interim: new FormControl('', [Validators.required]),
+  public DevCForm = new FormGroup({
+    majndc: new FormControl('', [Validators.required]),
+    dudc: new FormControl('', [Validators.required]),
+    Fonctiondc: new FormControl('', [Validators.required]),
+    categoriedc: new FormControl('', [Validators.required]),
+    suphierdc: new FormControl('', [Validators.required]),
+    Superdc: new FormControl('', [Validators.required]),
+    interidc: new FormControl('', [Validators.required]),
 
   });
 
-  public ProfilPForm = new FormGroup({
-    comptech: new FormControl('', [Validators.required]),
-    contenucomptech: new FormControl('', [Validators.required]),
-    comporgan: new FormControl('', [Validators.required]),
-    gestemps: new FormControl('', [Validators.required]),
-    nvisegestemp: new FormControl('', [Validators.required]),
-    isuivigestemp: new FormControl('', [Validators.required]),
-    compcompor: new FormControl('', [Validators.required]),
-    geek: new FormControl('', [Validators.required]),
-    autod: new FormControl('', [Validators.required]),
-    positivite: new FormControl('', [Validators.required]),
-    commintera: new FormControl('', [Validators.required]),
-    nvisecommintera: new FormControl('', [Validators.required]),
-    isuivicommintera: new FormControl('', [Validators.required]),
-    tracoll: new FormControl('', [Validators.required]),
-    nvisetracoll: new FormControl('', [Validators.required]),
-    isuivitracoll: new FormControl('', [Validators.required]),
-    resprob: new FormControl('', [Validators.required]),
-    nviseresprob: new FormControl('', [Validators.required]),
-    isuiviresprob: new FormControl('', [Validators.required]),
-    servexcell: new FormControl('', [Validators.required]),
-    nviseservexcell: new FormControl('', [Validators.required]),
-    isuiviservexcell: new FormControl('', [Validators.required]),
-    droit: new FormControl('', [Validators.required]),
-    contenudroit: new FormControl('', [Validators.required]),
-    devoir: new FormControl('', [Validators.required]),
-    contenudevoir: new FormControl('', [Validators.required]),
-    validation: new FormControl('', [Validators.required]),
-    contenuvalid: new FormControl('', [Validators.required]),
-    fonction: new FormControl('', [Validators.required]),
-    nompre: new FormControl('', [Validators.required]),
-    fonction1: new FormControl('', [Validators.required]),
-    nompre1: new FormControl('', [Validators.required]),
-    fonction2: new FormControl('', [Validators.required]),
-    nompre2: new FormControl('', [Validators.required]),
+  public ProfilPdcForm = new FormGroup({
+    comptechdc: new FormControl('', [Validators.required]),
+    contenucomptechdc: new FormControl('', [Validators.required]),
+    comporgandc: new FormControl('', [Validators.required]),
+    gestempsdc: new FormControl('', [Validators.required]),
+    nvisegestempdc: new FormControl('', [Validators.required]),
+    isuivigestempdc: new FormControl('', [Validators.required]),
+    compcompordc: new FormControl('', [Validators.required]),
+    autonomiedc: new FormControl('', [Validators.required]),
+    comminteradc: new FormControl('', [Validators.required]),
+    nvisecomminteradc: new FormControl('', [Validators.required]),
+    isuivicomminteradc: new FormControl('', [Validators.required]),
+    tracolldc: new FormControl('', [Validators.required]),
+    nvisetracolldc: new FormControl('', [Validators.required]),
+    isuivitracolldc: new FormControl('', [Validators.required]),
+    resprobdc: new FormControl('', [Validators.required]),
+    nviseresprobdc: new FormControl('', [Validators.required]),
+    isuiviresprobdc: new FormControl('', [Validators.required]),
+    servexcelldc: new FormControl('', [Validators.required]),
+    nviseservexcelldc: new FormControl('', [Validators.required]),
+    isuiviservexcelldc: new FormControl('', [Validators.required]),
+    raisconcepdc: new FormControl('', [Validators.required]),
+    nviseraisconcepdc: new FormControl('', [Validators.required]),
+    isuiviraisconcepdc: new FormControl('', [Validators.required]),
+    droitdc: new FormControl('', [Validators.required]),
+    contenudroitdc: new FormControl('', [Validators.required]),
+    devoirdc: new FormControl('', [Validators.required]),
+    contenudevoirdc: new FormControl('', [Validators.required]),
+    validationdc: new FormControl('', [Validators.required]),
+    contenuvaliddc: new FormControl('', [Validators.required]),
+    fonctiondc: new FormControl('', [Validators.required]),
+    nompredc: new FormControl('', [Validators.required]),
+    fonctiondc1: new FormControl('', [Validators.required]),
+    nompredc1: new FormControl('', [Validators.required]),
+    fonctiondc2: new FormControl('', [Validators.required]),
+    nompredc2: new FormControl('', [Validators.required]),
 
 
   });
@@ -75,18 +76,18 @@ export class ListingdevcComponent implements OnInit {
   }
 
   getallDevJ() {
-    this.dataService.get('Idproposte/getAllIdproposte.json').subscribe(res => {
-      this.Idproposte = res.data;
-      console.log("hello", this.Idproposte)
+    this.dataService.get('Idpropostedc/getAllIdpropostedc.json').subscribe(res => {
+      this.Idpropostedc = res.data;
+      console.log("hello", this.Idpropostedc)
 
 
     })
   }
 
   getallProfilP() {
-    this.dataService.get('Profilposte/getAllProfilposte.json').subscribe(res => {
-      this.Profilposte = res.data;
-      console.log("hello", this.Profilposte)
+    this.dataService.get('Profilpostedc/getAllProfilpostedc.json').subscribe(res => {
+      this.Profilpostedc = res.data;
+      console.log("hello", this.Profilpostedc)
 
 
     })
