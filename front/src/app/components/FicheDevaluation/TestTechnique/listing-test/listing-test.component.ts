dafbb0a4e1738ff1d384e1a0533ae401b9d51354
chourouk01 <127491299+chourouk01@users.noreply.github.com>
2023-03-29 11:26:32 +0100

    updates

--- front/src/app/components/FicheDevaluation/TestTechnique/listing-test/listing-test.component.ts
+++ front/src/app/components/FicheDevaluation/TestTechnique/listing-test/listing-test.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from 'src/app/shared/service/data.service';
 
 @Component({
   selector: 'app-listing-test',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingTestComponent implements OnInit {
 
-  constructor() { }
+  id: any;
+  public testtechniques
+
+  public testForm = new FormGroup({
+    duree: new FormControl('', [Validators.required]),
+
+    
+  });
+  constructor(
+    private dataService:DataService,
+    private router :Router,
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.gettestById();
   }
 
+  gettestById() {
+    this.id=this.route.snapshot.params['id'];
+    this.dataService.get('testtechniques/getTesttechnique.json?id='+this.id).subscribe(
+      res => {
+      this.testtechniques=res.data;
+      console.log("hello",this.testtechniques)
+
+      
+    })
+  }
+
+  edittest(){
+      
+    this.dataService.post('testtechniques/editTesttechnique.json?id='+this.id,this.testForm.value).subscribe(res=> {
+      console.log("hello",this.testForm.value)
+    this.router.navigate(['/listingtest'])
+    })
+  }
 }
