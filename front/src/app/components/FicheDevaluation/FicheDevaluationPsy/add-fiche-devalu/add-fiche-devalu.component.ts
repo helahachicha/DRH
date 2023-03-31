e3d15fc00e7f293b282fb195d0f2c32292cefa0a
chourouk01 <127491299+chourouk01@users.noreply.github.com>
2023-03-31 11:08:28 +0100

    updates

--- front/src/app/components/FicheDevaluation/FicheDevaluationPsy/add-fiche-devalu/add-fiche-devalu.component.ts
+++ front/src/app/components/FicheDevaluation/FicheDevaluationPsy/add-fiche-devalu/add-fiche-devalu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/shared/service/data.service';
 
 @Component({
   selector: 'app-add-fiche-devalu',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddFicheDevaluComponent implements OnInit {
 
-  constructor() { }
+  public infoficheForm = new FormGroup({
+    coutformahd: new FormControl('', [Validators.required]),
+    tocoformadt: new FormControl('', [Validators.required]),
+    locaespace: new FormControl('', [Validators.required]),
+    comax: new FormControl('', [Validators.required]),
+    tocout: new FormControl('', [Validators.required]),
+    chargeto: new FormControl('', [Validators.required]),
+    
+  });
+  public detailficheForm = new FormGroup({
+    coutformahd: new FormControl('', [Validators.required]),
+    tocoformadt: new FormControl('', [Validators.required]),
+    locaespace: new FormControl('', [Validators.required]),
+    comax: new FormControl('', [Validators.required]),
+    tocout: new FormControl('', [Validators.required]),
+    chargeto: new FormControl('', [Validators.required]),
+    
+  });
+  constructor(
+    private dataService:DataService,
+    private router :Router
+  ) { }
 
   ngOnInit(): void {
   }
-
+  
+   adddetailfichedevalu() {
+    //console.log("hello",this.coutForm.value)
+    this.dataService.post('Coutformaexternes/addCoutformaexterne.json',this.coutForm.value).subscribe(res=> {
+      this.router.navigate(['/coutlisting'])
+     
+      })
+   }
 }
