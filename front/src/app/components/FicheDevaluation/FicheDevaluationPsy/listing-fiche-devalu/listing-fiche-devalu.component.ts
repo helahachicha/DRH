f9f04335165be831a26b830e1f22ccc000deb256
chourouk01 <127491299+chourouk01@users.noreply.github.com>
2023-03-31 09:35:26 +0100

    updates

--- front/src/app/components/FicheDevaluation/FicheDevaluationPsy/listing-fiche-devalu/listing-fiche-devalu.component.ts
+++ front/src/app/components/FicheDevaluation/FicheDevaluationPsy/listing-fiche-devalu/listing-fiche-devalu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from 'src/app/shared/service/data.service';
 
 @Component({
@@ -9,12 +10,25 @@ import { DataService } from 'src/app/shared/service/data.service';
 })
 export class ListingFicheDevaluComponent implements OnInit {
 public Testtechniques
-  
+  id:any
   constructor(
     private dataService:DataService,
+    private router :Router,
+    private route:ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+  }
+
+  getcoutById() {
+    this.id=this.route.snapshot.params['id'];
+    this.dataService.get('coutformaexternes/getCoutformaexterne.json?id='+this.id).subscribe(
+      res => {
+      this.coutformaexternes=res.data;
+      console.log("hello",this.coutformaexternes)
+
+      
+    })
   }
   deletefichedevalu(id){
     this.dataService.delete('Profilpostes/deleteProfilposte.json?id='+id).subscribe(res => {
