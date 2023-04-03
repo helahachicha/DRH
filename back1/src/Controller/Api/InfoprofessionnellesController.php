3f495186d5ab6d301bdce8e4da843be036dae13c
bilel-2023 <127485761+bilel-2023@users.noreply.github.com>
2023-04-04 00:18:52 +0200

    Update InfoprofessionnellesController.php

--- back1/src/Controller/Api/InfoprofessionnellesController.php
+++ back1/src/Controller/Api/InfoprofessionnellesController.php
@@ -12,46 +12,53 @@ use App\Controller\AppController;
  */
 class InfoprofessionnellesController extends AppController
 {
-    public function insertInfoprofessionnelle(Request $request)
+    public function insertInfoprofessionnelle()
     {
-        $request -> validate([
-        ' daten ' =>'required ',
-        ' lieu' => 'required ',
-        ' nationalite ' =>'required ',
-        ' ncin' => 'required ',
-        ' delivreecin ' =>'required ',
-        ' datecin' => 'required ',
-        ' permis ' =>'required ',
-        ' datepermis ' =>'required ',
-        ' logement' => 'required ',
-        ' moyentransport ' =>'required ',
-        ' heure' => 'required ',
-        ' minute ' =>'required ',
-        ' situationfamiliale' => 'required ',
-        ' datemariedivorce ' =>'required ',
-        ]);
-        
-        $user = new Infoprofessionnelles ;
-        
-        $user->daten = $request->daten;
-        $user->lieu = $request->lieu;
-        $user->nationalite = $request->nationalite;
-        $user->ncin = $request->ncin;
-        $user->delivreecin = $request->delivreecin;
-        $user->datecin = $request->datecin;
-        $user->permis = json_encode($request->permis);
-        $user->datepermis = $request->datepermis;
-        $user->logement= json_encode($request->logement);
-        $user->moyentransport = json_encode($request->moyentransport);
-        $user->heure = $request->heure;
-        $user->minute = $request->minute;
-        $user->situationfamiliale= json_encode($request->situationfamiliale);
-        $user->datemariedivorce = $request->datemariedivorce;
-        
+        if ($this->request->is('post')) {
+            $this->request->allowMethod(['post']);
 
-        
-        $user->save();
-        return redirect()->back();
+            $this->request->validate([
+                'daten' => 'required',
+                'lieu' => 'required',
+                'nationalite' => 'required',
+                'ncin' => 'required',
+                'delivreecin' => 'required',
+                'datecin' => 'required',
+                'permis' => 'required',
+                'datepermis' => 'required',
+                'logement' => 'required',
+                'moyentransport' => 'required',
+                'heure' => 'required',
+                'minute' => 'required',
+                'situationfamiliale' => 'required',
+                'datemariedivorce' => 'required',
+            ]);
+
+            $user = $this->Infoprofessionnelles->newEmptyEntity();
+
+            $user->daten = $this->request->getData('daten');
+            $user->lieu = $this->request->getData('lieu');
+            $user->nationalite = $this->request->getData('nationalite');
+            $user->ncin = $this->request->getData('ncin');
+            $user->delivreecin = $this->request->getData('delivreecin');
+            $user->datecin = $this->request->getData('datecin');
+            $user->permis = json_encode($this->request->getData('permis'));
+            $user->datepermis = $this->request->getData('datepermis');
+            $user->logement= json_encode($this->request->getData('logement'));
+            $user->moyentransport = json_encode($this->request->getData('moyentransport'));
+            $user->heure = $this->request->getData('heure');
+            $user->minute = $this->request->getData('minute');
+            $user->situationfamiliale= json_encode($this->request->getData('situationfamiliale'));
+            $user->datemariedivorce = $this->request->getData('datemariedivorce');
+
+            if ($this->Infoprofessionnelles->save($user)) {
+                $this->Flash->success(__('Information professionnelle ajoutée avec succès.'));
+            } else {
+                $this->Flash->error(__('Erreur: l\'information professionnelle n\'a pas été ajoutée. Veuillez réessayer.'));
+            }
+
+            return $this->redirect(['action' => 'index']);
+        }
     }
 
 }
