6945a0009b6838584d1c0c115149da75a644538d
bilel-2023 <127485761+bilel-2023@users.noreply.github.com>
2023-04-03 15:00:00 +0200

    updates controller infoprofessionnelles

--- back1/src/Controller/Api/InfoprofessionnellesController.php
+++ back1/src/Controller/Api/InfoprofessionnellesController.php
@@ -44,10 +44,10 @@ class InfoprofessionnellesController extends AppController
 
         /* format data */
         if (1 == 1) {
-            //$querry=$this->request->getData();
-            //$data=json_decode($querry['data']); 
-            $data=$this->request->getData();
-            debug($data);die;
+            $querry=$this->request->getData();
+            $data=json_decode($querry['data']); 
+            //$data=$this->request->getData();
+            //debug($data);die;
 
         }
          /* create infoprofessionnelles entity */
