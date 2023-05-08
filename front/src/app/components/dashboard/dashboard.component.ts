849427f3c54f373d1326d469aace380003d42672
chourouk01 <127491299+chourouk01@users.noreply.github.com>
2023-05-08 14:35:05 +0100

    updates

--- front/src/app/components/dashboard/dashboard.component.ts
+++ front/src/app/components/dashboard/dashboard.component.ts
@@ -8,17 +8,25 @@ import { DataService } from 'src/app/shared/service/data.service';
 })
 export class DashboardComponent implements OnInit {
   public chart: any;
+  public chartt: any;
   public doughnut: any;
+  public doughnutt:any;
   public line:any;
   public Poly
   public matricescomp
+  public Polycom
+  public Employes
+
   constructor( private dataService: DataService,) { }
   
   ngOnInit(): void {
     this.createDoughnut();
+    this.createDoughnutpolyval();
     this.createLine();
     this.getpolyVal();
-    this.getPolyMatricecomp()
+    this.getPolyMatricecomp();
+    this.getpolycomp();
+    this.getemployer()
   }
   createChart(pol:any , mat:any){
     if (this.chart) {
@@ -33,16 +41,75 @@ export class DashboardComponent implements OnInit {
           {
             label: "Polyvalence",
             data: pol,
-            backgroundColor: '#fc9eb5'
+            backgroundColor: '#48cae4',
+            barThickness:15
+
           },
         ]
       },
       options: {
-        aspectRatio:3
+        aspectRatio:2
       }
       
     });
   }
+
+
+  createChartcomp(comp:any , emp:any){
+    if (this.chartt) {
+      this.chartt.destroy();
+    }
+    this.chartt = new Chart("MyChartcomp", {
+      type: 'bar', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: emp, 
+	       datasets: [
+          {
+            label: "Polycompetence",
+            data: comp,
+            backgroundColor: '#f07167',
+            barThickness:25
+          },
+        ]
+      },
+      options: {
+        aspectRatio:2
+      }
+      
+    });
+  }
+
+  createDoughnutpolyval(){
+    this.doughnutt = new Chart("MyChartDP", {
+      type: 'doughnut', //this denotes tha type of chart
+
+      data: {// values on X-Axis
+        labels: ['JUNIOR', 'CONFIRMÉ','SÉNIOR', ],
+        
+	       datasets: [{
+    label: 'My First Dataset',
+    data: [35,35,30],
+    backgroundColor: [
+      '#f05f5a',
+      '#feea33',
+      '#22ab4d',
+      			
+    ],
+    borderWidth:1,
+    circumference:180,
+    rotation:270
+  }],
+  
+      },
+      options: {
+        cutout: 120, //Définit la taille du trou central à 50%
+        
+      }
+
+    });
+  }
+
 
   createDoughnut(){
 
@@ -104,7 +171,7 @@ export class DashboardComponent implements OnInit {
   getpolyVal() {
     this.dataService.get('polyvalences/getallPolyVal.json').subscribe(res => {
       this.Poly = res.data.polyvalences;
-      console.log('All Poly', this.Poly)
+      //console.log('All Poly', this.Poly)
       this.createChart(this.Poly,this.matricescomp)
 
   })
@@ -113,8 +180,27 @@ export class DashboardComponent implements OnInit {
 getPolyMatricecomp() {
   this.dataService.get('Matricecompetences/getMatricecomp.json').subscribe(res => {
     this.matricescomp = res.data.matricecompetencesList;
-    console.log('All matricecomp', this.matricescomp)
+    //console.log('All matricecomp', this.matricescomp)
     this.createChart(this.Poly,this.matricescomp)
+
+})
+}
+
+
+getpolycomp() {
+  this.dataService.get('Polycompetences/getAllPolycompetence.json').subscribe(res => {
+    this.Polycom = res.data;
+    console.log('All Polycomp', this.Polycom)
+    this.createChartcomp(this.Polycom,this.Employes)
+
+})
+}
+
+getemployer() {
+this.dataService.get('Employes/getAllEmploye.json').subscribe(res => {
+  this.Employes = res.data;
+  console.log('All comp', this.Employes)
+  this.createChartcomp(this.Polycom,this.Employes)
 
 })
 }
