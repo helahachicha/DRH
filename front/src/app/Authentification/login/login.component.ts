668b9b87716b14a5ade9f1f1eea307ae34fde796
chourouk01 <127491299+chourouk01@users.noreply.github.com>
2023-05-23 20:33:21 +0100

    updates

--- front/src/app/Authentification/login/login.component.ts
+++ front/src/app/Authentification/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
   error:any = null ;
   
   public userForm = new FormGroup({
-    email: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(6)])
   });
   constructor(
